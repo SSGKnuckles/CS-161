265faa714675edabf4ac8e603a0f60783140dec3
Joshua Alcantar <joshua.alcantar+github@gmail.com>
2021-05-19 11:34:21 -0700

    new ch8 files from zybooks

---
+++ zybooks/charArrays8.7.cpp
@@ -0,0 +1,27 @@
+// Write a program that reads a list of words. Then, the program outputs those words and their frequencies. The input begins with an integer indicating the number of words that follow. Assume that the list will always contain less than 20 words. Each word will always contain less than 10 characters and no spaces.
+
+// Ex: If the input is:
+
+// 5 hey hi Mark hi mark
+// Then, the output is:
+
+// hey 1
+// hi 2
+// Mark 1
+// hi 2
+// mark 1
+// Hint: Use two arrays, one array for the strings and one array for the frequencies.
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+
+   char input[20][10];
+   int freq[20][1];
+
+   for (int i = 0)
+
+   return 0;
+}
