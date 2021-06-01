dfeca97ffac77a65ca82d788e247b97e4e70068c
Joshua Alcantar <joshua.alcantar+github@gmail.com>
2021-06-01 07:34:28 -0700

    Started a08.cpp

---
+++ assignments/a08-v1.0.cpp
@@ -0,0 +1,34 @@
+//*********************************************************
+//Author:       Alcantar, Joshua
+//Assignment:   Assignment 07
+//Date:         05/28/2021
+//Description:  Search a table for baby names [1880-1988]
+//Input:        User specifies year of interest
+//Output:       Greeting, ist of restaurants, search results,
+//               
+//Sources:      
+//***********************************************************
+
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <array>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    
+    ifstream inFile;
+
+
+
+
+    infile.open()
+
+
+
+    return 0
+}
